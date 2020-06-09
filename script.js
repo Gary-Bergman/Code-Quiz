8be8a1c1cfea061647fc71bd369bfaa5daa9cb4c
Gary Bergman <garybergman00@gmail.com>
2020-06-09 19:21:55 -0400

    Created function to iterate through questions.

--- script.js
+++ script.js
@@ -1,67 +1,106 @@
-// Timer
-
-// Select the id "myBtn" and assign it to a variable
+// Global variables
 var btnEl = document.querySelector("#startBtn");
-// Timer variable
+var submitBtn = document.querySelector("#submitBtn");
+var btn1 = document.querySelector("btn1");
 var timer = 120;
-var stopwatchTimer = 120;
-// Add an event listener so that when the button is clicked, it will start the timer
+
+
+// Event listener so that when the button is clicked, it will start the timer and ...
 btnEl.addEventListener("click", function () {
-    console.log("Button was clicked!!");
-    if (stopwatchTimer > 0) {
-        stopwatchTimer = setInterval(countdownTimer, 1000);
-    }
+    setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timer").textContent = timer;
+        }
+    }, 1000);
+
+    // When start button is clicked, my p1 should switch to hide
+    document.getElementById("p1").setAttribute("class", "hide");
+
+    // When start button is clicked, my answerBtns should switch to show
+    document.getElementById("btn1").setAttribute("class", "show");
+    document.getElementById("btn2").setAttribute("class", "show");
+    document.getElementById("btn3").setAttribute("class", "show");
+    document.getElementById("btn4").setAttribute("class", "show");
+
+    // When start button is clicked, my startBtn should switch to hide
+    document.getElementById("startBtn").setAttribute("class", "hide");
+
+    // When start button is clicked, my p2 should switch to show
+    document.getElementById("p2").setAttribute("class", "show");
+
+    // When start button is clicked, my submitBtn should switch to show
+    document.getElementById("submitBtn").setAttribute("class", "show");
+
+    // When start button is clicked execute questionFunc
+    questionFunc();
 });
-function countdownTimer() {
-    timer--;
-    document.querySelector("#timer").textContent = timer;
-}
-
-
-
-
-
-
-var newButton1 = document.createElement("button")
-
-newButton1.textContent = "<script>";
-//question 1
-
-// on click this will replace the html 
-startBtn.addEventListener("click", function () {
-    document.getElementById("question").innerHTML = "Inside which HTML element do we put the JavaScript?";
-
-    document.getElementById("answer").innerHTML = newButton1
-})
-
 
 
 var questionIndex = 0;
-//put in onclick function
-//change function name
-function q() {
-    questionIndex++
+// //put in onclick function
+function questionFunc() {
+    var question = [
+        {
+            questionText: "Inside which HTML element do we put the JavaScript?",
+            choice1: "<html>",
+            choice2: "<script>",
+            choice3: "<link>",
+            choice4: "<body>",
+            answer: "<script>"
+            //answer needs to match string
+        },
+        {
+            questionText: "What is the corrct syntax for referring to an external script?",
+            choice1: "<script href=...>",
+            choice2: "<script name=...>",
+            choice3: "<script src=...>",
+            choice4: "<script style=...>",
+            answer: "<script src=...>"
+        },
+        {
+            questionText: "How do you create a function in JavaScript??",
+            choice1: "Function",
+            choice2: "function = myFunction()",
+            choice3: "Function = myFunction()",
+            choice4: "function : myFunction()",
+            answer: "function = myFunction()"
+        },
+        {
+            questionText: "How does a FOR loop start?",
+            choice1: "for (i = 0++)",
+            choice2: "for (i > 0; i = 0)",
+            choice3: "for (i = 0, i > 5, i++)",
+            choice4: "for (i = 0; i > 5; i++)",
+            answer: "for (i = 0; i > 5; i++)"
+        },
+        {
+            questionText: "How can you add a comment in a JavaScript?",
+            choice1: "// This is a comment.",
+            choice2: "<!-- This is a comment. -->",
+            choice3: "<-- This is a comment. --!>",
+            choice4: "-This is a comment.-",
+            answer: "// This is a comment."
+        }
+    ]
+    if (questionIndex == 0) {
+        document.getElementById("question").textContent = question[questionIndex].questionText;
+        document.getElementById("btn1").textContent = question[questionIndex].choice1;
+        document.getElementById("btn2").textContent = question[questionIndex].choice2;
+        document.getElementById("btn3").textContent = question[questionIndex].choice3;
+        document.getElementById("btn4").textContent = question[questionIndex].choice4;
+    }
     //update display for text in buttons and question
-    // set text cont by id answer document.getElementById("Btn2").textContent = question[questionIndex].choice2
+    // set text content by id answer
+    submitBtn.addEventListener("click", function () {
+        document.getElementById("question").textContent = question[questionIndex].questionText;
+        document.getElementById("btn1").textContent = question[questionIndex].choice1;
+        document.getElementById("btn2").textContent = question[questionIndex].choice2;
+        document.getElementById("btn3").textContent = question[questionIndex].choice3;
+        document.getElementById("btn4").textContent = question[questionIndex].choice4;
+        questionIndex++;
+    })
+
+    btn1
 }
 
-var question = [
-    {
-        questionText: "What is blah blah blah?",
-        choice1: "Ablah",
-        choice2: "Bsdflkjdsfj",
-        choice3: "C",
-        choice4: "D",
-        answer: "Ablah"
-        //answer needs to match string
-    },
-    {
-        questionText: "What is the other thing?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: "D"
-    },
-
-];
